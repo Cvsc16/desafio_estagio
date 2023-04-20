a947a535b872afe0c258dc103191fd6c3625aede
android3 <caio.vcosta@sou.unaerp.edu.br>
2023-04-19 22:18:22 -0300

    Sexto Commit

--- app/src/main/java/unaerp/com/desafio/ActivityVagas.kt
+++ app/src/main/java/unaerp/com/desafio/ActivityVagas.kt
@@ -2,14 +2,20 @@ package unaerp.com.desafio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ActivityVagas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vagas)
-        val vaga1 = Vaga("Desenvolvedor Mobile","Unaerp","Ribeirão Preto",
+        val vaga1 = Vaga("Desenvolvedor Mobile","PicPay","Ribeirão Preto",
         "Remoto","31/03/2023","1500,00")
 
-
+        val rvVagas: RecyclerView? = findViewById(R.id.rvVagas)
+        rvVagas?.layoutManager = LinearLayoutManager(this)
+        rvVagas?.adapter = VagasAdapter(
+            listOf(vaga1)
+        )
     }
 }
--- app/src/main/java/unaerp/com/desafio/VagasAdapter.kt
+++ app/src/main/java/unaerp/com/desafio/VagasAdapter.kt
@@ -1,30 +1,32 @@
 package unaerp.com.desafio
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class VagasAdapter(list: List<Vaga>): RecyclerView.Adapter<VagasAdapter.VagaViewHolder>() {
+class VagasAdapter(val vagaList: List<Vaga>): RecyclerView.Adapter<VagasAdapter.VagaViewHolder>() {
 
     inner class VagaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun setInfo(vaga: Vaga) {
-            val nomeEmpresaTv: TextView = TODO()
+            val nomeEmpresaTv: TextView = itemView.findViewById(R.id.nome_empresa)
             nomeEmpresaTv.text = vaga.empresa
 
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VagaViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.cardvaga, parent, false)
+        return VagaViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return vagaList.size
     }
 
     override fun onBindViewHolder(holder: VagaViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.setInfo(vagaList[position])
     }
 
 }
