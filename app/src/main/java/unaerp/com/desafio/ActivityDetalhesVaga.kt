c4d757901989b2ae1196eb11ffa264d9b16b7c04
mlcosta <caio.vcosta@sou.unaerp.edu.br>
2023-04-25 11:10:45 -0300

    commit 25 04 23

--- app/src/main/java/unaerp/com/desafio/ActivityDetalhesVaga.kt
+++ app/src/main/java/unaerp/com/desafio/ActivityDetalhesVaga.kt
@@ -6,11 +6,14 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telephony.PhoneNumberFormattingTextWatcher
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.constraintlayout.widget.ConstraintLayout
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
@@ -23,5 +26,17 @@ class ActivityDetalhesVaga : AppCompatActivity() {
         setContentView(R.layout.activity_detalhesvaga)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        val buttonDetalhes = findViewById<Button>(R.id.button_detalhes)
+        val buttonDetalhes2 = findViewById<Button>(R.id.button_detalhes2)
+
+        buttonDetalhes.setOnClickListener {
+            buttonDetalhes2.setBackgroundResource(0)
+            buttonDetalhes.setBackgroundResource(R.drawable.button_detalhesvaga)
+        }
+
+        buttonDetalhes2.setOnClickListener {
+            buttonDetalhes2.setBackgroundResource(R.drawable.button_detalhesvaga)
+            buttonDetalhes.setBackgroundResource(0)
+        }
     }
 }
