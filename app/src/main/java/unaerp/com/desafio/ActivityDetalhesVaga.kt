65b6c11394d53dd3888f2c5c8c29453ff38ef5ff
mlcosta <caio.vcosta@sou.unaerp.edu.br>
2023-04-25 11:56:18 -0300

    commit 25 04 23

--- app/src/main/java/unaerp/com/desafio/ActivityDetalhesVaga.kt
+++ app/src/main/java/unaerp/com/desafio/ActivityDetalhesVaga.kt
@@ -1,23 +1,9 @@
 package unaerp.com.desafio
 
-import android.app.DatePickerDialog
-import android.content.res.ColorStateList
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telephony.PhoneNumberFormattingTextWatcher
-import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.constraintlayout.widget.ConstraintLayout
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import java.util.Date
-import java.util.Locale
 
 class ActivityDetalhesVaga : AppCompatActivity() {
 
